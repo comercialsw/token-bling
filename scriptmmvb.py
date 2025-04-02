6081de0c2054d9ff2f6b498b96ad4a81ff31859b
comercialsw <comercial@smartwaybr.com.br>
2025-04-02 16:38:51 -0300

    Update scriptmmvb.py

--- scriptmmvb.py
+++ scriptmmvb.py
@@ -7,7 +7,7 @@ import os
 # Defina seus dados diretamente aqui:
 CLIENT_ID = "f449c93d4d4d8fcd9f5b819dbae2120414e0a139"
 CLIENT_SECRET = "8a321c10140f6824e49cbc3da73f2626ad83956b628b023577f7c9d44daf"
-AUTHORIZATION_CODE = "2732d01ef430a53b6b61429fee6d461014b3cfc8"
+AUTHORIZATION_CODE = "16319b45b37751790c8ad0a315721ed412873dac"
 
 TOKEN_FILE = "tokensmmvb.json"  # Salvará o token na raiz do repositório GitHub
 
